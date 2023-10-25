5f8c8cfc53d88e09c344415e6ecdd2eac7b3e296
Yevhenns <gifestum@gmail.com>
2023-10-25 15:47:32 +0300

    drinks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'objectstorage.eu-frankfurt-1.oraclecloud.com',
+        port: '',
+        pathname: '/n/fr0trzcie6nl/b/pizza/o/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
