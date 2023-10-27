bb0f0b985c49968dd12d4258a8e78c13f1db9b93
Yevhenns <gifestum@gmail.com>
2023-10-27 15:36:23 +0300

    render path

--- src/api/getItems.ts
+++ src/api/getItems.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getItems = async (route: string) => {
   try {
     const response = await axios.get(
-      `http://130.162.51.139/api/${route}`
+      `https://xata-magnata-server.onrender.com/api/${route}`
     );
     return response.data.data.result;
   } catch (error: any) {
