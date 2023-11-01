0ebf7d85d96b6fcb824acd8a815f5ba2a0322142
Yevhenns <gifestum@gmail.com>
2023-11-01 15:44:29 +0200

    maps api key

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+      process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
--- src/components/addressesComponent/SimpleMap/SimpleMap.tsx
+++ src/components/addressesComponent/SimpleMap/SimpleMap.tsx
@@ -1,31 +1,40 @@
 import React, { FC } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { BiMap } from 'react-icons/bi';
 
 interface Props {
-  text: string;
   lat: number;
   lng: number;
 }
 
-const GoogleMaps: FC<Props> = ({ text }) => <div>{text}</div>;
+const GoogleMaps: FC<Props> = () => (
+  <div>
+    <BiMap style={{ color: 'red', fontSize: '32px' }} />
+  </div>
+);
 
 export default function SimpleMap() {
   const defaultProps = {
     center: {
-      lat: 48.38208,
-      lng: 34.99455,
+      lat: 48.4211840588917,
+      lng: 35.00988524052585,
     },
-    zoom: 11,
+    zoom: 12,
   };
+
+  const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
   return (
     <div style={{ height: '300px', width: '600px' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
+        bootstrapURLKeys={{ key: `${API_KEY}` }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
       >
-        <GoogleMaps lat={48.38208} lng={34.99455} text="My Marker" />
+        <GoogleMaps lat={48.407641691999906} lng={35.00017456670064} />
+        <GoogleMaps lat={48.42555310026097} lng={35.022020774202005} />
+        <GoogleMaps lat={48.43342155185263} lng={35.00226293664556} />
       </GoogleMapReact>
     </div>
   );
