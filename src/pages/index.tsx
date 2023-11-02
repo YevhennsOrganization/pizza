66deea84560318b413ca704eb8fb16dde27bbf25
Yevhenns <gifestum@gmail.com>
2023-11-02 12:37:56 +0200

    replace carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,9 +27,9 @@ export const Home: FC = () => {
       </Head>
       <Section>
         <Container>
-          <CarouselComponent />
           <Heading>Новинки</Heading>
           <div style={{ height: '50px' }}>{isLoading && <Loader />}</div>
+          <CarouselComponent />
         </Container>
       </Section>
     </>
