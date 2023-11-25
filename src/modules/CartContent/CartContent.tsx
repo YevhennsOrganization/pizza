542310ec148ca72f4b1567576faf6748bc32fe28
Yevhenns <gifestum@gmail.com>
2023-11-25 18:14:11 +0200

    moved fn

--- src/modules/CartContent/CartContent.tsx
+++ src/modules/CartContent/CartContent.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Empty from '@/components/Empty/Empty';
 import CartForm from '@/components/CartForm/CartForm';
 import CartList from '@/components/CartList/CartList';
@@ -8,8 +8,6 @@ interface Props {
   deleteCartItem: (id: string) => void;
   deleteAllProducts: () => void;
   openModal: () => void;
-  totalPayment: number;
-  order: TOrdered;
 }
 
 const CartContent: FC<Props> = ({
@@ -17,9 +15,23 @@ const CartContent: FC<Props> = ({
   deleteCartItem,
   deleteAllProducts,
   openModal,
-  totalPayment,
-  order,
 }) => {
+  const [totalPayment, setTotalPayment] = useState(0);
+
+  const order: TOrdered = filledCart.map(item => {
+    return {
+      title: item.title,
+      quantity: item.quantity,
+    };
+  });
+
+  useEffect(() => {
+    const result = filledCart
+      .map((element: { totalPrice: number }) => element.totalPrice)
+      .reduce((acc: number, val: number) => acc + val, 0);
+    setTotalPayment(result);
+  }, [filledCart]);
+
   return (
     <>
       {filledCart.length > 0 ? (
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,9 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import Heading from '@/UI/basic/Heading/Heading';
-import CartForm from '@/components/CartForm/CartForm';
+import CartContent from '@/modules/CartContent/CartContent';
 import FinalModal from '@/components/FinalModal/FinalModal';
-import CartList from '@/components/cart/CartList/CartList';
-import Empty from '@/components/Empty/Empty';
 import PagesWrapper from '@/components/PagesWrapper/PagesWrapper';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import {
@@ -15,11 +13,9 @@ import {
   getError,
 } from '@/redux/cart/cartSlice';
 import css from '../styles/pages/Cart.module.scss';
-import CartContent from '@/modules/CartContent/CartContent';
 
 const Cart: FC = () => {
   const [open, setOpen] = useState(false);
-  const [totalPayment, setTotalPayment] = useState(0);
 
   const dispatch = useAppDispatch();
   const filledCart = useAppSelector(getFilledCart);
@@ -40,20 +36,6 @@ const Cart: FC = () => {
     setOpen(false);
   };
 
-  const order: TOrdered = filledCart.map(item => {
-    return {
-      title: item.title,
-      quantity: item.quantity,
-    };
-  });
-
-  useEffect(() => {
-    const result = filledCart
-      .map((element: { totalPrice: number }) => element.totalPrice)
-      .reduce((acc: number, val: number) => acc + val, 0);
-    setTotalPayment(result);
-  }, [filledCart]);
-
   return (
     <PagesWrapper title="Nostra pizza - Кошик">
       <div className={css.cartWrapper}>
@@ -63,8 +45,6 @@ const Cart: FC = () => {
           deleteCartItem={deleteCartItem}
           deleteAllProducts={deleteAllProducts}
           openModal={openModal}
-          totalPayment={totalPayment}
-          order={order}
         />
         {open && (
           <FinalModal
