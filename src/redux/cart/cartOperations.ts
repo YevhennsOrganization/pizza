f7741d8e4affc4f21f0ead9f8bff3aa61080b054
Yevhenns <gifestum@gmail.com>
2023-12-05 15:21:10 +0200

    added env url

--- src/redux/cart/cartOperations.ts
+++ src/redux/cart/cartOperations.ts
@@ -1,5 +1,8 @@
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const BASE_URL = process.env.BASE_URL;
+axios.defaults.baseURL = BASE_URL;
 
 export const sendOrder = createAsyncThunk<
   number,
@@ -9,12 +12,9 @@ export const sendOrder = createAsyncThunk<
   }
 >('cart/sendOrder', async (order, { rejectWithValue }) => {
   try {
-    const res = await axios.post(
-      'https://api-pizza.blaize.technology/api/send_email',
-      {
-        body: JSON.stringify(order),
-      }
-    );
+    const res = await axios.post('/send_email', {
+      body: JSON.stringify(order),
+    });
     return res.status;
   } catch (error: any) {
     return rejectWithValue(error.message as string);
--- src/redux/products/productsOperations.ts
+++ src/redux/products/productsOperations.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+const BASE_URL = process.env.BASE_URL;
+axios.defaults.baseURL = BASE_URL;
+
 export const getProducts = createAsyncThunk<
   TProductsArr,
   void,
@@ -9,9 +12,7 @@ export const getProducts = createAsyncThunk<
   }
 >('products/getProductsAll', async (_, { rejectWithValue }) => {
   try {
-    const res = await axios.get(
-      'https://api-pizza.blaize.technology/api/products'
-    );
+    const res = await axios.get('/products');
     return res.data.data.result as TProductsArr;
   } catch (error: any) {
     return rejectWithValue(error.message as string);
