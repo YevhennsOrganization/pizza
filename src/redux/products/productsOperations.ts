d2ede77231d31d66528f8bbfe289d3e80945a05a
Yevhenns <gifestum@gmail.com>
2023-12-23 15:26:12 +0200

    removed axios from products

--- src/redux/products/productsOperations.ts
+++ src/redux/products/productsOperations.ts
@@ -1,8 +1,6 @@
-import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const BASE_URL = process.env.BASE_URL;
-axios.defaults.baseURL = BASE_URL;
 
 export const getProducts = createAsyncThunk<
   TProductsArr,
@@ -12,9 +10,10 @@ export const getProducts = createAsyncThunk<
   }
 >('products/getProductsAll', async (_, { rejectWithValue }) => {
   try {
-    const res = await axios.get('/products');
-    return res.data.data.result as TProductsArr;
+    const res = await fetch(`${BASE_URL}products`);
+    const data: TResponse = await res.json();
+    return data.data.result;
   } catch (error: any) {
-    return rejectWithValue(error.message as string);
+    return rejectWithValue(error.message);
   }
 });
